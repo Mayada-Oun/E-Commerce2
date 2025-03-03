2c18aabdce5fe2c01c8f351876a3aec7794e550c
Mayada Oun <mayadaoun@std.mans.edu.eg>
2025-03-03 06:24:01 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Component from "./Component"
+
+const App = () => {
+  return (
+    <div>
+      <Component></Component>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Component() {
+  return (
+    <div>
+      <div><h2>Hello World</h2> </div>
+    </div>
+  )
+}
+
+export default Component
+
+    
