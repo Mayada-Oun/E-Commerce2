9f6279cee0a08e668fc940e30cbfb0b90a8bd903
Mayadaabdelsallam <mayadaoun@std.mans.edu.eg>
2025-03-07 14:48:08 +0200

    Initial commit

--- src/Component.jsx
+++ src/Component.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 function Component() {
   return (
     <div>
-      <div><h2>Hello World</h2> </div>
+      <div>
+        <h2>Hello World</h2> </div>
+        <div><h1>Faculty of Computer Science</h1></div>
     </div>
   )
 }
 
 export default Component
 
-    
